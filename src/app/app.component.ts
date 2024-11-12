6248b478533e1765fb1cf7cbc24cea8b64c2bebf
Bindhiya C Philip <bindhiya.philip@experionglobal.local>
2024-11-12 16:04:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyButtonComponent } from "./ui/my-button/my-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MyButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'day2';
+  title = 'Bindhiya';
 }
